9e7cf5ece6fc61e28fcb29ca1b6a0876c1a6cbc0
Mark Triggs <mark@dishevelled.net>
2010-06-06 12:37:40 +1000

    Fix to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject expenses "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  :namespaces [expenses]
   :main Expenses)
